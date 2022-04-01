7c478c5675c0cd69e81cec4e740504f336babd88
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-03-31 18:15:10 -0600

    Métodos agregarALaLista y getLastItMiha documentados.

--- src/itmiha/Main.java
+++ src/itmiha/Main.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 
 public class Main {
 
+    /**
+     * Método que agrega los números del input a una lista
+     *
+     * @param a Lista vacía por llenar
+     */
     public static List agregarALaLista(List<Integer> a){
         Scanner scn = new Scanner(System.in);
         do {
@@ -11,6 +16,11 @@ public class Main {
         return a;
     }
 
+    /**
+     * Método que revisa cuál es el número It-Miha más alto que buscamos.
+     *
+     * @param a Lista a revisar
+     */
     public static Integer getLastItMiha(List <Integer> a){
         a.remove(0);
         Integer max = Collections.max(a);
