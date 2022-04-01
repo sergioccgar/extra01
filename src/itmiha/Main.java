d3f4871c7ee09188e309165fb54b1ff9918bb0bd
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-03-31 20:00:05 -0600

    Agregadas pruebas unitarias del teorema fundamental de la aritmética. Agregado teorema fundamental de la aritmética como método para descomponer un número en sus factores primos y meter los mismos en una lista.

--- src/itmiha/Main.java
+++ src/itmiha/Main.java
@@ -4,6 +4,30 @@ import java.io.IOException;
 public class Main {
 
     /**
+     * Método que descompone un número en sus factores primos y agrega a estos
+     * a una lista.
+     *
+     * @param n Número a descomponer
+     * @param l Lista donde se guardan los factores primos
+     */
+    public static List<Integer> teoremaFundArim(int n, List<Integer> l){
+        l.clear();
+        int cociente = -1;
+        if (n == 1){
+            l.add(1);
+            return l;
+        } else {
+            int i = 2;
+            while (cociente != 1){
+                if (n%i == 0) {
+                    cociente = n = n / i;
+                    l.add(i);
+                } else i++;
+            }
+            return l;
+        }
+    }
+    /**
      * Método que agrega los números del input a una lista
      *
      * @param a Lista vacía por llenar
@@ -25,12 +49,32 @@ public class Main {
      * @param a Lista a revisar
      */
     public static Integer getLastItMiha(List <Integer> a){
-        Integer numCasos = a.get(0);
-        a.remove(0);
-        Integer max = Collections.max(a);
-        a.add(0,numCasos);
-        return max;
+        if (a.size() == 1) {
+            return -1;
+        } else {
+            Integer numCasos = a.get(0);
+            a.remove(0);
+            Integer max = Collections.max(a);
+            a.add(0,numCasos);
+            return max;
+        }
     }
+
+    /**
+     * Método que consigue todos los primeros n números It-Miha. Sólo guarda en
+     * una lista final los números deseados.
+     *
+     * @param n Último número It-Miha a conseguir.
+     * @param a Lista de números It-Miha buscados.
+     * @param b Lista donde se guardarán los números It-Miha encontrados.
+     */
+
+     // public static List<Integer> findMihas(Integer n, List<Integer> a, List<Integer> b){
+     //     int contador = 0;
+     //     for (Integer i = 1; i <= n){
+     //         if (imt)
+     //     }
+     // }
 
     public static void main (String args[]){
 
--- src/itmiha/test.java
+++ src/itmiha/test.java
@@ -11,6 +11,7 @@ public class test extends Main {
     public static void main (String args[]){
         System.out.println("Ingresa los numeros a la lista, termina con Ctrl+D");
         List<Integer> a = new ArrayList<Integer>();
+        List<Integer> b = new ArrayList<Integer>();
         agregarALaLista(a);
         System.out.println("La lista es:");
         for (int i = 0; i < a.size(); i++){
@@ -29,5 +30,105 @@ public class test extends Main {
                 System.out.print(a.get(i) + ", ");
             }
         }
+        List<Integer> c = new ArrayList<Integer>();
+        System.out.println("Teorema Fundamental de la Aritmética");
+        teoremaFundArim(1,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(2,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(3,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(4,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(5,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(6,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(7,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(8,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(9,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        teoremaFundArim(10,c);
+        System.out.println("La lista es:");
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        // System.out.println("Los números It-Miha que buscas son:");
+        // List<Integer> c = new ArrayList<Integer>();
+        // c.add(1);
+        // c.add(2);
+        // c.add(4);
+        // c.add(12);
+        // c.add(371);
+        // findMiha(371, c, b);
     }
 }
