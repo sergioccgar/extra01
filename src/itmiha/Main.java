f51d3104e1e9bf8767ad856ebcf6a4333ff39e62
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-03-31 21:24:48 -0600

    Agregados métodos para obtener los primeros n números it-miha y para sacar sólo los de los índices deseados de esa lista. Todo ya funciona. Resta el main.

--- src/itmiha/Main.java
+++ src/itmiha/Main.java
@@ -27,6 +27,32 @@ public class Main {
             return l;
         }
     }
+
+    /**
+     * Método que descompone un número en sus factores primos y agrega a estos
+     * a una lista.
+     *
+     * @param n Número a descomponer
+     *
+     */
+    public static List<Integer> teoremaFundArim2(int n){
+        List<Integer> l = new ArrayList<Integer>();
+        int cociente = -1;
+        if (n == 1){
+            l.add(1);
+            return l;
+        } else {
+            int i = 2;
+            while (cociente != 1){
+                if (n%i == 0) {
+                    cociente = n = n / i;
+                    l.add(i);
+                } else i++;
+            }
+            return l;
+        }
+    }
+
     /**
      * Método que agrega los números del input a una lista. No considera
      * repeticiones, salvo el primer número.
@@ -37,13 +63,31 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         Integer first = Integer.parseInt(scn.nextLine());
         Integer aux;
-        do {
+        while (scn.hasNextLine()) {
             aux = Integer.parseInt(scn.nextLine());
             if (!a.contains(aux)) a.add(aux);
-        } while (scn.hasNextLine());
+        }
         a.add(0,first);
         return a;
     }
+
+    /**
+     * Método que ve si en una lista de enteros hay al menos una repetición.
+     *
+     * @param l Lista a revisar
+     */
+     public static boolean hayRepes(List<Integer> l){
+         Integer aux;
+         boolean repeated = false;
+         for (int i = 0; i < l.size(); i++){
+             aux = l.get(i);
+             l.remove(i);
+             repeated = l.contains(aux);
+             l.add(i,aux);
+             if (repeated == true) break;
+         }
+         return repeated;
+     }
 
     /**
      * Método que revisa cuál es el número It-Miha más alto que buscamos. No
@@ -66,20 +110,46 @@ public class Main {
     }
 
     /**
-     * Método que consigue todos los primeros n números It-Miha. Sólo guarda en
-     * una lista final los números deseados.
+     * Método que consigue todos los primeros n números It-Miha.
      *
      * @param n Último número It-Miha a conseguir.
-     * @param a Lista de números It-Miha buscados.
-     * @param b Lista donde se guardarán los números It-Miha encontrados.
+     *
      */
 
-     // public static List<Integer> findMihas(Integer n, List<Integer> a, List<Integer> b){
-     //     int contador = 0;
-     //     while (contador < n) {
-     //
-     //     }
-     // }
+     public static List<Integer> findFirstnMihas(Integer n){
+         int contador = 0;
+         int i = 1;
+         List<Integer> mihas = new ArrayList<Integer>();
+         mihas.add(-1);
+         while (contador < n) {
+             if (hayRepes(teoremaFundArim2(i))){
+                 i++;
+             } else {
+                 contador++;
+                 mihas.add(i);
+                 i++;
+             }
+         }
+         return mihas;
+     }
+
+    /**
+     * Método que dada una lista de números It-Miha, devuelve una lista con
+     * los números deseados (por ejemplo: el primero, el doceavo, etc.)
+     *
+     * @param l Lista de números It-Miha
+     * @param m Lista de números It-Miha deseados.
+     *
+     */
+
+    public static List<Integer> giveMeSumItMihas(List<Integer> l, List<Integer> m) {
+        m.remove(0);
+        List<Integer> askedItMihas = new ArrayList<Integer>();
+        for (Integer i: m){
+            askedItMihas.add(l.get(i));
+        }
+        return askedItMihas;
+    }
 
     public static void main (String args[]){
 
--- src/itmiha/test.java
+++ src/itmiha/test.java
@@ -41,6 +41,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(2,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -50,6 +51,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(3,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -59,6 +61,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(4,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -68,6 +71,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(5,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -77,6 +81,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(6,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -86,6 +91,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(7,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -95,6 +101,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(8,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -104,6 +111,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(9,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -113,6 +121,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(10,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -122,6 +131,7 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         teoremaFundArim(234567,c);
         System.out.println("La lista es:");
         for (int i = 0; i < c.size(); i++){
@@ -131,13 +141,80 @@ public class test extends Main {
                 System.out.print(c.get(i) + ", ");
             }
         }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
+        System.out.println("La lista es:");
+        c = teoremaFundArim2(234567);
+        for (int i = 0; i < c.size(); i++){
+            if (i+1 == c.size()) {
+                System.out.println(c.get(i) + ".");
+            } else {
+                System.out.print(c.get(i) + ", ");
+            }
+        }
+        System.out.println("La descomposición factorial tiene valores repetidos: " + hayRepes(c));
         // System.out.println("Los números It-Miha que buscas son:");
-        // List<Integer> c = new ArrayList<Integer>();
-        // c.add(1);
-        // c.add(2);
-        // c.add(4);
-        // c.add(12);
-        // c.add(371);
-        // findMiha(371, c, b);
+        List<Integer> mix = new ArrayList<Integer>();
+        // mix.add(10);
+        // mix.add(1);
+        // mix.add(2);
+        // mix.add(3);
+        // mix.add(4);
+        // mix.add(5);
+        // mix.add(6);
+        // mix.add(7);
+        // mix.add(8);
+        // mix.add(9);
+        // mix.add(10);
+        // mix = findMihas(getLastItMiha(mix), mix);
+        // for (int i = 0; i < mix.size(); i++){
+        //     if (i+1 == mix.size()) {
+        //         System.out.println(mix.get(i) + ".");
+        //     } else {
+        //         System.out.print(mix.get(i) + ", ");
+        //     }
+        // }
+        // mix.clear();
+        // mix.add(5);
+        // mix.add(1);
+        // mix.add(2);
+        // mix.add(4);
+        // mix.add(12);
+        // mix.add(371);
+        // mix = findMihas(getLastItMiha(mix), mix);
+        // for (int i = 0; i < mix.size(); i++){
+        //     if (i+1 == mix.size()) {
+        //         System.out.println(mix.get(i) + ".");
+        //     } else {
+        //         System.out.print(mix.get(i) + ", ");
+        //     }
+        // }
+        System.out.println("Los primeros 10 números it-miha son: ");
+        mix = findFirstnMihas(10);
+        for (int i = 0; i < mix.size(); i++){
+            if (i+1 == mix.size()) {
+                System.out.println(mix.get(i) + ".");
+            } else {
+                System.out.print(mix.get(i) + ", ");
+            }
+        }
+        System.out.println("Los números it-miha que pediste son:");
+        mix.clear();
+        mix.add(5);
+        mix.add(1);
+        mix.add(2);
+        mix.add(4);
+        mix.add(12);
+        mix.add(371);
+        List<Integer> firstN = findFirstnMihas(getLastItMiha(mix));
+        List<Integer> finale = giveMeSumItMihas(firstN,mix);
+        for (int i = 0; i < finale.size(); i++){
+            if (i+1 == finale.size()) {
+                System.out.println(finale.get(i) + ".");
+            } else {
+                System.out.print(finale.get(i) + ", ");
+            }
+        }
+
+
     }
 }
