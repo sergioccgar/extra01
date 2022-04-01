c8775014d0e930dd4a0a5575813687ec45918707
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-03-31 18:03:53 -0600

    Agregada prueba que verifica que se agreguen bien los números a la lista. Agregada prueba que verifica que el número It-Miha buscado más sea el correcto. Listos métodos que lo verifican.

--- src/itmiha/Main.java
+++ src/itmiha/Main.java
@@ -1,9 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.IOException;
 
 public class Main {
 
+    public static List agregarALaLista(List<Integer> a){
+        Scanner scn = new Scanner(System.in);
+        do {
+            a.add(Integer.parseInt(scn.nextLine()));
+        } while (scn.hasNextLine());
+        return a;
+    }
+
+    public static Integer getLastItMiha(List <Integer> a){
+        a.remove(0);
+        Integer max = Collections.max(a);
+        return max;
+    }
+
     public static void main (String args[]){
-        
+
     }
 }
--- src/itmiha/test.java
+++ src/itmiha/test.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Pruebas unitarias para el ejercicio It-Miha
  *
@@ -7,6 +9,17 @@
 public class test extends Main {
 
     public static void main (String args[]){
-        
+        System.out.println("Ingresa los numeros a la lista, termina con Ctrl+D");
+        List<Integer> a = new ArrayList<Integer>();
+        agregarALaLista(a);
+        System.out.println("La lista es:");
+        for (int i = 0; i < a.size(); i++){
+            if (i+1 == a.size()) {
+                System.out.println(a.get(i) + ".");
+            } else {
+                System.out.print(a.get(i) + ", ");
+            }
+        }
+        System.out.println("El número It-Miha más alto que buscas es el " + getLastItMiha(a) + "° número It-Miha");
     }
 }
